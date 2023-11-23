af5ebe0e22e3b7d49fc1381bc81ff02069d9fc95
ahmed said <primaryahmedsaid@gmail.com>
2023-11-23 19:16:29 +0200

    update: start auth pages

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,25 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Shapes } from "lucide-react";
+import Link from "next/link";
+
+export default function SigninPage() {
+  return (
+    <div className="container pt-20">
+      <Shapes className="inline-block" size={60} />
+      <h1 className="text-2xl font-bold">Create an account</h1>
+      <Link
+        href="sign-in"
+        className={cn(
+          buttonVariants({
+            variant: "link",
+            size: "sm",
+          }),
+          "p-0 text-blue-500",
+        )}
+      >
+        sign-in if you already have an account
+      </Link>
+    </div>
+  );
+}
