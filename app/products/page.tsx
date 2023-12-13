74d8c03abfce847f735d03fe050c7adc50d2de85
Ahmed Said <101777385+ahmeds189@users.noreply.github.com>
2023-12-13 12:36:54 +0200

    feat: dynamic title & categories

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,13 +1,26 @@
 import ProductReel from "@/components/product-reel";
 
-// TODO: dinamic peoducts pased on nav link
+// TODO: dynamic products based on nav link
+type Props = {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
 
-export default function ProductsPage() {
-  <div className="container">
+const parse = (param: string | string[] | undefined) => {
+  return typeof param === "string" ? param : undefined;
+};
+
+export default function ProductsPage({ searchParams }: Props) {
+  const category = parse(searchParams.category);
+  const sort = parse(searchParams.sort);
+
+  return (
     <ProductReel
-      title="
-    Browse high quality products"
-      query={{ category: "", limit: 40, sort: "desc" }}
+      title={category || "Browse high quality products"}
+      query={{
+        category,
+        limit: 40,
+        sort: sort === "desc" || sort === "asc" ? sort : undefined,
+      }}
     />
-  </div>;
+  );
 }
